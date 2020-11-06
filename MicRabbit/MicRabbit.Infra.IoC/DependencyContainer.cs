655d0fab03d5e684c95f0bfcec16fe5bb1cb8d55
Sheyin Nkom <coldasiyce@gmail.com>
2020-11-06 01:45:42 +0100

    Initial Commit of Infrastruncture Dependency Project

---
+++ MicRabbit/MicRabbit.Infra.IoC/DependencyContainer.cs
@@ -0,0 +1,18 @@
+﻿using MicRabbit.Domain.Core.Bus;
+using MicRabbit.Infra.Bus;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MicRabbit.Infra.IoC
+{
+    public class DependencyContainer
+    {
+        public static void RegisterServices(IServiceCollection services)
+        {
+            // Domain Bus
+            services.AddTransient<IEventBus, RabbitMQBus>();
+        }
+    }
+}
